3dd6d464000369662d90e7383f0d0307f129960f
Giadapasqualini <75312702+Giadapasqualini@users.noreply.github.com>
2020-12-01 12:34:18 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Calcola il doppio di un numero fornito da tastiera
+#include <iostream>
+using namespace std;
+
+int main () {
+ int numero;
+ int doppio;
+ cout << "immetti numero" << endl;
+ cin >> numero;
+ doppio = numero *2;
+
+  std::cout << "il doppio di" << numero
+            << "è" <<doppio<< endl;
+} 
